bb2e12442fe2d0657facb9992fa413db3c5e6fb1
Jack <965555169@qq.com>
2023-08-09 23:31:04 +0800

    优化代码

--- index.js
+++ index.js
@@ -4,39 +4,33 @@ const cheerio = require('cheerio');
 const { program } = require('commander');
 const { Scheduler } = require('./scheduler');
 
-const TOTAL_COUNT = 100;
+const TOTAL_COUNT = 100; // 爬取总次数
 
-let scheduler = undefined;
+let scheduler = undefined; // 调度器
 
-const fetchData = async url => {
-  try {
-    const res = await axios.get(url);
-    return cheerio.load(res.data);
-  } catch (error) {
-    console.error(`Error in fetching data from ${url}`);
-    return null;
-  }
-};
-
+// 获取网站标题，并打印序号
 const getTitle = (index, $) => {
   console.log(`${index} 网站标题:`, $('title').text());
 };
 
-const runSpider = async (concurrency, url) => {
+const runSpider = (concurrency, url) => {
+  // 初始化调度器
   scheduler = new Scheduler(concurrency);
+  // 添加任务
   for (let i = 0; i < TOTAL_COUNT; i++) {
-    scheduler.add(async () => {
-      const $ = await fetchData(url);
-      $ && getTitle(i, $);
+    scheduler.add(() => {
+      return axios.get(url).then(res => {
+        getTitle(i, cheerio.load(res.data));
+      });
     });
   }
 };
 
 program
-  .requiredOption('-c, --concurrent <Number>')
-  .arguments('<url>')
+  .requiredOption('-c, --concurrent <Number>') // 并发数
+  .arguments('<url>') // 爬取的网站
   .action((url, option) => {
-    runSpider(Number(option.concurrent), url);
+    runSpider(Number(option.concurrent), url); // 开始爬取
   });
 
 program.parse(process.argv);
